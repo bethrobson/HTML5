7c4210135328fc901e76d66734a74d008fa718b9
Teddy Huff <theodorejhuff@gmail.com>
2012-12-12 14:25:14 -0600

    Resize; Non 360 Dependent
    Added function to dynamically resize the canvas tag on resize/load (set
    to 80%)
    
    Also added multipliers to the data. Values do not have to equal 360 now
    to create a full circle. I have added a 'multiplier' variable that will
    multiple all necessary values to make them render large enough to close
    a gap.
    
    Tweaked font size and spacing.

--- PieChart/piechart.js
+++ PieChart/piechart.js
@@ -25,6 +25,16 @@ function PieChart(id, o) {
 		this.includeLabels = o.includeLabels;
 	}
 	this.data = o.data ? o.data : [30, 70, 45, 65, 20, 130]; // in degrees
+	
+	var dataLength = this.data.length,
+	totalCount = 0;
+	for(i = 0; i < dataLength; i++)
+	{
+		totalCount += this.data[i]
+	}
+	this.multiplier = 360/totalCount; 
+	
+	
 	this.labels = o.labels ? o.labels : ["First", "Second", "Third", "Fourth", "Fifth", "Sixth"];
 	this.colors = o.colors ? o.colors : [
         	["#bbddb3", "#1d8e04"], // green
@@ -55,14 +65,17 @@ PieChart.prototype = {
 	},
 
 	drawSegment: function(canvas, context, i, size, isSelected, includeLabels) {
+		size *= this.multiplier;
 		var self = this;
 		context.save();
 		var centerX = Math.floor(canvas.width / 2);
 		var centerY = Math.floor(canvas.height / 2);
 		radius = Math.floor(canvas.width / 2);
-		
+
 		var startingAngle = self.degreesToRadians(self.sumTo(self.data, i));
+		startingAngle *= this.multiplier;
 		var arcSize = self.degreesToRadians(size);
+		
 		var endingAngle = startingAngle + arcSize;
 
 		context.beginPath();
@@ -73,7 +86,7 @@ PieChart.prototype = {
 		isSelected ? 
 			context.fillStyle = self.colors[i][1] :
 			context.fillStyle = self.colors[i][0];
-
+			
 		context.fill();
 		context.restore();
 
@@ -87,16 +100,16 @@ PieChart.prototype = {
 		context.save();
 		var x = Math.floor(canvas.width / 2);
 		var y = Math.floor(canvas.height / 2);
-		var angle = self.degreesToRadians(self.sumTo(self.data, i));
-
+		var angle = self.degreesToRadians(self.sumTo(self.data, i)*this.multiplier);
+		
 		context.translate(x, y);
 		context.rotate(angle);
 		context.textAlign = "right";
-		var fontSize = Math.floor(canvas.height / 25);
+		var fontSize = Math.floor(canvas.height / 30);
 		context.font = fontSize + "pt Helvetica";
 
 		var dx = Math.floor(canvas.width * 0.5) - 10;
-		var dy = Math.floor(canvas.height * 0.05);
+		var dy = Math.floor(canvas.height * 0.04);
 		context.fillText(self.labels[i], dx, dy);
 
 		context.restore();
@@ -106,7 +119,7 @@ PieChart.prototype = {
 		var self = this;
 		var context = this.canvas.getContext("2d");
 		var size = self.data[i];
-
+		size *= this.multiplier;
 		self.drawSegmentLabel(this.canvas, context, i, size, false);
 	},
 
@@ -125,4 +138,3 @@ PieChart.prototype = {
 
 
 }
-
